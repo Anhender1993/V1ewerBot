f74f2fddaad3ee5310450df9781c4d7302d47439
Andrew Henderson <Andrew.Henderson@atlasschool.com>
2025-04-25 18:17:53 -0500

    final implementation with manual refresh

---
+++ README.md
@@ -0,0 +1,2 @@
+# V1ewer
+This is a bot created for discord using Python.  The idea is you can give it a list of streamers to announce when they're going live
---
+++ start_all.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "C:\Users\bioni\iCloudDrive\Atlas Projects\Personal\V1ewerbot\V1ewer\V1ewerBot"
+
+start cmd /k "python bot.py"
+start cmd /k "python dashboard.py"
+
+echo.
+echo 🚀 V1ewerbot and Dashboard are now running in two separate windows!
+pause
