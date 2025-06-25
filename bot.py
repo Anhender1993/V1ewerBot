b0832de93dac0280562061ae593f3d514b28d5e4
Anhender1993 <andrew.henderson1993@outlook.com>
2025-06-24 19:30:24 -0500

    scrapped previous XP/Roles idea for now.  Reverting to previous build so that I can start again later without losing current functionality

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
 TWITCH_CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
-GUILD_ID = int(os.getenv('GUILD_ID'))
-CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+GUILD_ID = int(os.getenv('GUILD_ID', 0))
+CHANNEL_ID = int(os.getenv('CHANNEL_ID', 0))
 
 intents = discord.Intents.default()
 intents.guilds = True
